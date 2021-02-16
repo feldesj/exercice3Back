b4066625612ebf390069f58cc5edb20af598c9f5
feldesj <feldesj@users.noreply.github.com>
2021-02-16 16:47:04 -0500

    initial

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+const __dirname = path.resolve();
+
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, "build")));
+
+const classes = ["warrior", "mage", "ranger"];
+
+app.get("/newClass", (req, res) => {
+  const index = Math.floor(Math.random() * Math.floor(3));
+  console.log(index);
+  const yourNewClass = classes[index];
+  res.send(yourNewClass);
+});
+console.log(__dirname);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(80);
+console.log("backend started");
